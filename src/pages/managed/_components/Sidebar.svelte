6b04b3edfbe6148b2937e95d65dc9519529e3d45
Kefah Issa <kefah@edraj.io>
2021-01-25 03:00:37 +0300

    update

---
+++ src/pages/managed/_components/Sidebar.svelte
@@ -0,0 +1,11 @@
+<script>
+	let title_height;
+</script>
+
+<div bind:clientHeight="{title_height}">
+	<h6 class="text-info p-1 my-0"> Managed Layout </h6>
+	<hr class="my-0" />
+</div>
+<div style="height: calc(100% - {title_height + 4}px); overflow: hidden auto;">
+	<p> some content here </p>
+</div>
--- src/pages/managed/_fallback.svelte
+++ src/pages/managed/_fallback.svelte
@@ -3,4 +3,17 @@
 
 </script>
 
-<h4> The main thing goes here </h4>
+<h6> The main thing goes here </h6>
+<p>
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed vel tristique elit, sit amet aliquet sapien. Integer quis mauris ac nulla sollicitudin aliquam quis at lectus. Sed congue, arcu non porttitor rhoncus, orci quam commodo quam, a luctus purus orci sit amet lectus. Etiam eget commodo orci, nec dapibus enim. Proin congue lectus dolor, in lobortis felis sagittis eget. Pellentesque vel eros ac tellus ullamcorper eleifend vel in nulla. Nunc suscipit mauris quis mattis rutrum. Interdum et malesuada fames ac ante ipsum primis in faucibus. Aenean vitae sem felis. Sed vitae magna nulla. Integer accumsan blandit eros quis cursus. Donec eu nunc sapien. Sed euismod nisi in massa accumsan, quis luctus orci gravida. Quisque consequat convallis libero, vitae volutpat justo suscipit quis.
+
+In dignissim massa enim, non gravida dui imperdiet sit amet. Maecenas a nunc nisl. Quisque tincidunt vestibulum nisi sit amet vestibulum. Aliquam eu neque ac ex egestas ultrices sit amet sed nulla. Quisque non ullamcorper arcu. In ornare justo in dolor placerat, a bibendum erat viverra. Duis luctus augue eu purus molestie ullamcorper. Praesent ut quam vel magna suscipit sodales. Phasellus id sodales magna, eget commodo ex.
+
+Suspendisse porta, nunc vel posuere blandit, eros elit congue nisl, vel pharetra ipsum ligula a massa. Sed at diam eros. Nullam quam tortor, volutpat non ornare dignissim, porttitor non lectus. Etiam fermentum nunc lorem, quis suscipit elit mattis sed. Quisque magna velit, placerat et efficitur sed, elementum sed urna. Ut mollis erat non auctor consequat. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+
+Vestibulum fringilla feugiat lectus, eget eleifend massa rhoncus nec. Vestibulum semper, neque vel tincidunt faucibus, magna neque cursus massa, in interdum quam erat eget ex. Sed eu felis imperdiet, porttitor erat quis, rutrum massa. Nam euismod tortor odio, quis tincidunt nulla consequat efficitur. Integer lacus sem, vestibulum sollicitudin magna vitae, aliquet efficitur lectus. Pellentesque ultrices velit quis justo bibendum lobortis. Maecenas at lectus tincidunt, molestie purus sit amet, condimentum justo. Curabitur auctor a justo in rhoncus. Nullam mattis tortor urna. In vel urna ac sem mollis consequat quis eu augue. Cras lobortis ultricies ligula a pharetra.
+
+Nunc vel eros a nisl porta efficitur. Curabitur et posuere lectus, quis ornare odio. Nulla rutrum, enim ut dapibus egestas, nibh arcu blandit massa, ullamcorper tristique neque ligula vel massa. Aenean vestibulum quam metus, id laoreet felis ultrices nec. Nunc sit amet lobortis massa. Sed porta turpis lorem. Sed dictum imperdiet enim at volutpat. Proin consectetur sit amet est in placerat. Nunc eget diam non sapien iaculis vestibulum sit amet vestibulum nisi. Curabitur convallis non libero id mollis. Curabitur sed consequat elit. Phasellus consequat, urna vel dictum sollicitudin, nunc nisl ultrices odio, non ornare urna tellus in nulla.
+
+</p>
