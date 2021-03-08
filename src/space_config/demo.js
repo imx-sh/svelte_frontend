a5fb277d29c444247ef0e0735b9b1642c5b4a4ae
Alpine i0 imx <alpine@imx.sh>
2021-03-08 22:38:59 +0000

    update

---
+++ src/space_config/demo.js
@@ -0,0 +1,11 @@
+export const website = {
+  title: 'demo',
+	short_name: 'demo',
+	display_name: 'Demo',
+	description: 'Demo Space',
+  languages: {'ar': 'العربية', 'en': 'English'},
+  backend: 'https://demo.imx.sh',
+	space_name: 'demo',
+	about: '<p> About section of the Demo space </p>',
+	footer: 'demo space -- powered by <strong> IMX </strong>'
+};
